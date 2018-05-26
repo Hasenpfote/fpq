8d2c6fab3453051836f75b24785fdf98a9054b36
Hasenpfote <Hasenpfote@gmail.com>
2018-05-26 20:51:29 +0900

    Change settings

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def _long_description():
 if __name__ == '__main__':
     setup(
         name='fpq',
-        version='0.4.0',
+        version='0.5.0',
         description='',
         long_description=_long_description(),
         author='Hasenpfote',
