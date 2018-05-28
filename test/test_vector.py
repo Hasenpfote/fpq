0f0f65e4ccd5bfc8af5bc494cf5f37c1e0d52d40
Hasenpfote <Hasenpfote@gmail.com>
2018-05-28 16:37:27 +0900

    Change some tests

--- test/test_vector.py
+++ test/test_vector.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
 from unittest import TestCase
 from itertools import chain
 import numpy as np
@@ -128,6 +129,7 @@ class TestVector(TestCase):
             self.assertTrue(isinstance(actual, tuple))
             self.assertTrue(np.array_equal(actual, expected[i]))
 
+    @unittest.skipIf(np.__version__ < '1.10.0', 'not supported in this numpy version')
     def test_encoding_decoding_between_vec16_and_uint32(self):
         dtypes = (np.float16, np.uint32)
         nbits = 10
@@ -138,7 +140,6 @@ class TestVector(TestCase):
         dec = decode_uint_to_vec(enc, dtype=dtypes[0], nbits=nbits)
         self.assertTrue(isinstance(dec, np.ndarray))
         self.assertTrue(dec.dtype == dtypes[0])
-        print(dec.dtype, dtypes[0])
         self.assertTrue(np.allclose(dec, expected, rtol=1e-01, atol=1e-02))
 
         expected = np.array([[10, 20, 30],
