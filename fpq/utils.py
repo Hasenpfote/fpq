f0b580177bf965cc37d0952bb4a6f72c1eeb9f53
Hasenpfote <Hasenpfote@gmail.com>
2018-06-09 21:47:28 +0900

    Reduce Broadcasting

--- fpq/utils.py
+++ fpq/utils.py
@@ -18,11 +18,11 @@ def remove_component(x, *, indices):
     return ma.compressed().reshape(shape)
 
 
-def remap(value, src_min, src_max, dst_min, dst_max):
+def remap(x, src_min, src_max, dst_min, dst_max):
     '''Maps values from [`src_min`, `src_max`]  to [`dst_min`, `dst_max`].
 
     Args:
-        value: The incoming value to be converted.
+        x: The incoming value to be converted.
         src_min: Lower bound of the value current range.
         src_max: Upper bound of the value current range.
         dst_min: Lower bound of the value target range.
@@ -33,7 +33,7 @@ def remap(value, src_min, src_max, dst_min, dst_max):
 
     Examples:
     '''
-    return (((value - src_min) / (src_max - src_min)) * (dst_max - dst_min)) + dst_min
+    return (x - src_min) * ((dst_max - dst_min) / (src_max - src_min)) + dst_min
 
 
 def _can_express_norm(nbits, dtype):
